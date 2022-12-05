729661c3641461c834c7deba9d8f90783b8a469d
Nicolás González <nigonalz@gmail.com>
2022-12-05 19:34:42 +0100

    Added "extract page" and "crop pages"

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-user_input = input("Enter pdf file address: ")
-input_pdf = PdfFileReader(open(f"{user_input}.pdf", "rb"))
 
-for i in range(input_pdf.numPages):
-    output = PdfFileWriter()
-    output.addPage(input_pdf.getPage(i))
-    with open("document-page%s.pdf" % i, "wb") as outputStream:
-        output.write(outputStream)
+class Document:
+    def __init__(self, path):
+        self.path = path
 
-print(f"Done!\n")
+    def separatePages(self):
+        input_pdf = PdfFileReader(open(f"{self.path}", "rb"))
+
+        for i in range(input_pdf.numPages):
+            output = PdfFileWriter()
+            output.addPage(input_pdf.getPage(i))
+            with open("document-page%s.pdf" % i, "wb") as outputStream:
+                output.write(outputStream)
+
+    def extractPage(self):
+        input_pdf = PdfFileReader(open(f"{self.path}", "rb"))
+        page = int(input("Gimme the page to extract: "))
+        output = PdfFileWriter()
+
+        output.addPage(input_pdf.getPage(page))
+        with open("document-page%s.pdf" % page, "wb") as outputStream:
+            output.write(outputStream)
+            outputStream.close()
+
+
+myDocument = Document(input("Gimme the path: "))
+myDocument.extractMultiplePages()
